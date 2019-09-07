79a559674a40b179f7f7aebcd81b7bb58dd00c3b
onyeka <onynwamba@gmail.com>
2019-09-07 01:24:47 +0100

    Giving each picture a designated card image

--- Card.py
+++ Card.py
@@ -1,3 +1,7 @@
+import os
+from tkinter import PhotoImage
+assets_folder = os.getcwd() + "/assets"
+
 class Card:
     def __init__(self,suit,value):
         self.suit = suit
@@ -6,3 +10,10 @@ class Card:
     def __repr__(self):
         return " of ".join((self.value, self.suit))
 
+    @classmethod
+    def get_back_file(cls):
+        cls.back = PhotoImage(file=assets_folder+"backs/Card-Back-03.png")
+        return cls.back
+
+    def get_file(self):
+        return self.img
