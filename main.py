d86983906e7f506ddd53cf25d24aa3c23da489fd
onyeka <onynwamba@gmail.com>
2019-09-07 01:28:02 +0100

    Fixed minor bugs

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from .Game import Game
+from .Hand import Hand
+from .Deck import Deck
+
+class GameState:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+
+        self.player_hand = Hand()
+        self.dealer_hand = Hand(dealer=True)
+
+        for i in range(2):
+            self.player_hand.add_card(self.deck.deal())
+            self.dealer_hand.add_card(self.deck.deal())
+
+        self.has_winner = ''
 
 if __name__ == "__main__":
     game = Game()
